032393e6710d3aa34519d282df50dfd2148721f7
Vihanga Munasinghe <149761186+VihangaMunasinghe@users.noreply.github.com>
2025-09-01 23:27:11 +0530

    test template cofig implementation

---
+++ mcq_marking/app/main.py
@@ -0,0 +1,6 @@
+from mcq_marking.app.templateconfig.config import get_config
+
+
+bubble_coordinates, warped_img, result_img = get_config('/Users/vihangamunasinghe/WebProjects/DSE Project/mcq-ocr/samples/templates/1.jpg')
+
+print(bubble_coordinates)
