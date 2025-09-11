3e4cdad63454a350a0ab1c8e3d4821bb223acbba
Geesan Munasinghe <sugithgeesan@gmail.com>
2025-09-11 14:31:59 +0530

    Configured for docker compose

--- index_recognision/task_queue/rabbitMQ.py
+++ index_recognision/task_queue/rabbitMQ.py
@@ -2,14 +2,13 @@ import os
 import pika
 import json
 ################################ Configurations ###################
-HOST = os.getenv('RABBITMQ_HOST', 'localhost')
-PORT = os.getenv('RABBITMQ_PORT', 5672)
+URL = os.getenv('RABBITMQ_URL', 'amqp://localhost:5672')
 
 
 
 ################################ Functions #######################
 def send_message(queue: str, data: dict, routing_key: str = ''):
-    outgoing_connection = pika.BlockingConnection(pika.ConnectionParameters(host=HOST, port=PORT))
+    outgoing_connection = pika.BlockingConnection(pika.URLParameters(URL))
     message = json.dumps(data)
     outgoing_channel = outgoing_connection.channel()
     outgoing_channel.queue_declare(queue=queue)
@@ -17,9 +16,9 @@ def send_message(queue: str, data: dict, routing_key: str = ''):
     outgoing_connection.close()
 
 def start_consuming(queue: str, callback: callable):
-    incoming_connection = pika.BlockingConnection(pika.ConnectionParameters(host=HOST, port=PORT))
+    incoming_connection = pika.BlockingConnection(pika.URLParameters(URL))
     incoming_channel = incoming_connection.channel()
     incoming_channel.queue_declare(queue=queue)
     incoming_channel.basic_consume(queue=queue, on_message_callback=callback, auto_ack=True)
-    print(f" [*] Waiting for tasks in {queue}")
+    print(f" [*] Waiting for tasks in {queue}",flush=True)
     incoming_channel.start_consuming()
