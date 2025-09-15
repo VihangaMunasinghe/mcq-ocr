f98ebc92ac6939aff358a1bd3fb98468cfe6b1ff
Geesan Munasinghe <sugithgeesan@gmail.com>
2025-09-15 23:38:55 +0530

    frontend_template_edit

--- next_frontend/components/Layout/MainLayout.tsx
+++ next_frontend/components/Layout/MainLayout.tsx
@@ -12,9 +12,9 @@ export default function MainLayout({
   return (
     <div className="flex h-screen bg-gray-50">
       <Sidebar />
-      <div className="flex-1 flex flex-col md:ml-20 lg:ml-64">
+      <div className="flex-1 flex flex-col">
         <Navbar />
-        <main className="flex-1 overflow-y-auto p-4 sm:p-6 lg:p-8">
+        <main className="flex-1 overflow-y-auto overflow-x-auto p-4 sm:p-6 lg:p-8">
           {children}
         </main>
       </div>
